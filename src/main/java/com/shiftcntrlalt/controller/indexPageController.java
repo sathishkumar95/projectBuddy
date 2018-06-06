a23d45369c96cd1b7dd4a650efb886002bc79594
sathish <sathishp.railway@gmail.com>
2018-06-06 16:46:16 +0530

    This is a tester phase

---
+++ src/main/java/com/shiftcntrlalt/controller/indexPageController.java
@@ -0,0 +1,13 @@
+package com.shiftcntrlalt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexPageController {
+
+    @RequestMapping(value = "/")
+    public String indexPage(){
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hey broooo</title>
+</head>
+<body>
+
+</body>
+</html>
